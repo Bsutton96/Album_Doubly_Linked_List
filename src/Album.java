95fa44df39eae629aa15daf152d9705cb6b18153
benjaminsutton <ben.sutton96@outlook.com>
2023-03-29 19:54:43 -0400

    Implemented Album Method

--- src/Album.java
+++ src/Album.java
@@ -1,3 +1,21 @@
-public class Album {
+public class Album implements Comparable<Album> {
+
+    private int id;
+
+    private String artist;
+
+    private String title;
+
+    private int numSongs;
+
+    public Album(id, artist, title, numSongs) {
+        this.id = id;
+        this.artist = artist;
+        this.title = title;
+        this.numSongs = numSongs;
+
+    }
+
+
 
 }
