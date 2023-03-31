d328477ec8c337ce9a31d86677589b6ffc1f5dc8
benjaminsutton <ben.sutton96@outlook.com>
2023-03-30 20:12:19 -0400

    Implemented shuffle and partition tests

--- Tests/AlbumTest.java
+++ Tests/AlbumTest.java
@@ -27,17 +27,14 @@ class AlbumTest {
     public void testAppend() {
         DoublyLinkedList<Integer> testList = new DoublyLinkedList<>();
 
-        //append testList, add 3 nodes
         Node node1 = testList.append(1);
         Node node2 = testList.append(2);
         Node node3 = testList.append(3);
 
-        //Verify nodes are not null
         Assertions.assertNotNull(node1);
         Assertions.assertNotNull(node2);
         Assertions.assertNotNull(node3);
 
-        //Print to verify value of nodes
         System.out.println(String.valueOf(node1));
         System.out.println(String.valueOf(node2));
         System.out.println(String.valueOf(node3));
@@ -90,6 +87,48 @@ class AlbumTest {
         list.append(2);
         list.append(3);
         Assertions.assertEquals("1 -> 2 -> 3 -> NULL", list.toString());
-        
+
+    }
+
+    @Test
+    void testShuffle() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.insert(0, 1);
+        list.insert(1, 2);
+        list.insert(2, 3);
+        list.insert(3, 4);
+        list.insert(4, 5);
+        list.insert(5, 6);
+        list.shuffle();
+
+        System.out.println(list.toString());
+
+        Assertions.assertEquals(6, list.size);
+
+        boolean isShuffled = false;
+        Node<Integer> current = list.head;
+        for (int i = 1; i <= 6; i++) {
+            if (current.data != i) {
+                isShuffled = true;
+                break;
+            }
+            current = current.next;
+        }
+        Assertions.assertTrue(isShuffled);
+    }
+
+    @Test
+    void testPartition() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.append(5);
+        list.append(2);
+        list.append(10);
+        list.append(8);
+        list.append(3);
+
+        DoublyLinkedList<Integer> partitioned = list.partition(5);
+
+        Assertions.assertEquals("2 -> 3 -> NULL", list.toString());
+        Assertions.assertEquals("5 -> 10 -> 8 -> NULL", partitioned.toString());
     }
 }
